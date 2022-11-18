906c6c0c78be7404e3c7250cc07c9d3f3d710c7e
Chia <lj1635@126.com>
2022-11-18 19:55:32 +0800

    初始化

---
+++ src/test/java/com/gzu/system/SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gzu.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
