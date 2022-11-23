4dcd5556eb52597ac65867a7a63675081153e3f4
Chia <lj1635@126.com>
2022-11-23 20:37:10 +0800

    合并前修改

--- src/main/java/com/gzu/system/service/UserLoginService.java
+++ src/main/java/com/gzu/system/service/UserLoginService.java
@@ -1,30 +1,23 @@
-package com.gzu.system.service.impl;
+package com.gzu.system.service;
 
 import com.gzu.system.mapper.UserLoginMapper;
 import com.gzu.system.pojo.UserLogin;
-import com.gzu.system.service.IUserLoginService;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.Resources;
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 
 @Service
-public class UserLoginService implements IUserLoginService {
+public class UserLoginService {
 
     private UserLoginMapper mapper;
 
 
     /**
      * 利用java原生的摘要实现SHA256加密
+     *
      * @param str 密码
      * @return 加密后的报文
      */
@@ -45,6 +38,7 @@ public class UserLoginService implements IUserLoginService {
 
     /**
      * 将byte转为16进制
+     *
      * @param bytes
      * @return
      */
@@ -63,7 +57,6 @@ public class UserLoginService implements IUserLoginService {
     }
 
 
-    @Override
     public int register(String username, String password, String type) {
         //1.根据用户名查看用户是否被占用
         UserLogin userLoginBySelect = mapper.selectByUserName(username);
@@ -73,13 +66,13 @@ public class UserLoginService implements IUserLoginService {
         }
 
         //2.没被占用的用户名，将其密码哈希处理
-        String hashPassword=getSHA256StrJava(password);
+        String hashPassword = getSHA256StrJava(password);
 
         //3.检查type类型是否正确
-        UserLogin.type myType=null;
-        try{
-            myType= Enum.valueOf(UserLogin.type.class, type.toUpperCase());
-        }catch (Exception e){
+        UserLogin.type myType = null;
+        try {
+            myType = Enum.valueOf(UserLogin.type.class, type.toUpperCase());
+        } catch (Exception e) {
             return 2;
         }
 
@@ -87,28 +80,27 @@ public class UserLoginService implements IUserLoginService {
 //            return 2;
 //        }
         //3.插入user_login,根据情况报错
-        try{
+        try {
             UserLogin userLogin = new UserLogin();
             userLogin.setUsername(username);
             userLogin.setPasswordHash(hashPassword);
             userLogin.setType(myType);
             mapper.insert(userLogin);
 
-        }catch (Exception e){
+        } catch (Exception e) {
             return 2;
         }
         return 0;
     }
 
-    @Override
     public int login(String username, String password) {
 
         UserLogin userLogin = null;
         try {
             //1.查询用户是否存在
-            userLogin=mapper.selectByUserName(username);
+            userLogin = mapper.selectByUserName(username);
             //用户不存在，返回4
-            if(userLogin==null){
+            if (userLogin == null) {
                 return 4;
             }
             //2.用户存在,检查密码是否正确
@@ -117,7 +109,7 @@ public class UserLoginService implements IUserLoginService {
                 return 3;
             }
             //3.密码正确，根据用户类型返回值
-            switch (userLogin.getType()){
+            switch (userLogin.getType()) {
                 case PEOPLE:
                     return 0;
                 case PLACE:
@@ -126,7 +118,7 @@ public class UserLoginService implements IUserLoginService {
                     return 2;
             }
 
-        }catch (Exception e){
+        } catch (Exception e) {
             //4.出现其他错误
             return 5;
         }
@@ -135,9 +127,6 @@ public class UserLoginService implements IUserLoginService {
         return 5;
 
     }
-
-
-
 
 
 }
--- src/test/java/com/gzu/system/SystemApplicationTests.java
+++ src/test/java/com/gzu/system/SystemApplicationTests.java
@@ -1,26 +1,13 @@
 package com.gzu.system;
 
-import com.gzu.system.mapper.UserLoginMapper;
-import com.gzu.system.pojo.UserLogin;
-import com.gzu.system.service.IUserLoginService;
-import com.gzu.system.service.impl.UserLoginService;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 @SpringBootTest
 class SystemApplicationTests {
-    @Test
-    public void testReg(){
 
+    @Test
+    void contextLoads() {
     }
+
 }
