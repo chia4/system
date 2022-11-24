23a74bded8628aefce67a2cab8b8c2412f7b0843
飞牛天使 <1198115482@qq.com>
2022-11-24 15:19:28 +0800

    merge prepare

--- src/main/java/com/gzu/system/SystemApplication.java
+++ src/main/java/com/gzu/system/SystemApplication.java
@@ -1,11 +1,14 @@
 package com.gzu.system;
 
+import com.gzu.system.service.IUserLoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class SystemApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(SystemApplication.class, args);
--- src/test/java/com/gzu/system/SystemApplicationTests.java
+++ src/test/java/com/gzu/system/SystemApplicationTests.java
@@ -1,26 +1,73 @@
 package com.gzu.system;
 
-import com.gzu.system.mapper.UserLoginMapper;
-import com.gzu.system.pojo.UserLogin;
 import com.gzu.system.service.IUserLoginService;
-import com.gzu.system.service.impl.UserLoginService;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 @SpringBootTest
 class SystemApplicationTests {
+
+    @Autowired
+    private IUserLoginService userLoginService;
+
+    @Test
+    public void testTypeError(){
+        //类型错误，返回2
+        int re = userLoginService.register("lwjb", "123", "PEOPLEs");
+        System.out.println(re);
+    }
+
+
+    @Test
+    public void testRegUserExists(){
+        //注册用户被占用，返回1
+        int re = userLoginService.register("lwj", "123", "PEOPLE");
+        System.out.println(re);
+    }
+
+
     @Test
     public void testReg(){
+        //lwj 123 PEOPLE
+        //userLoginService.register("lwj","123","PEOPLE");
+        //lwjt 123 PLACE
+        //userLoginService.register("lwjt","123","PLACE");
+        //lwjtt 123 AGENCY
+        userLoginService.register("lwjtt","123","AGENCY");
+    }
+
+
+    @Test
+    public void testLoginSuccess(){
+        //成功登录，用户类型为PEOPLE，返回0
+        int lwj = userLoginService.login("lwj", "123");
+        int lwjt = userLoginService.login("lwjt", "123");
+        int lwjtt = userLoginService.login("lwjtt", "123");
+        System.out.println(lwj);
+        System.out.println(lwjt);
+        System.out.println(lwjtt);
+    }
+
+    @Test
+    public void testLoginPasswordError(){
+        //密码错误，返回3
+        int lwj = userLoginService.login("lwj", "1234");
+        System.out.println(lwj);
+    }
+
+    @Test
+    public void testLoginUserNotExists(){
+        //用户不存在，返回4
+        int lwja = userLoginService.login("lwja", "123");
+        System.out.println(lwja);
+    }
+
+    @Test
+    public void testOtherError(){
 
     }
+
+
+
 }
