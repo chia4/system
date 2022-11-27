255e77c1578e9031e374ef0d2afde6e7d9aaf603
Chia <lj1635@126.com>
2022-11-27 19:03:03 +0800

    大众完善信息页面完善

--- src/main/java/com/gzu/system/SystemApplication.java
+++ src/main/java/com/gzu/system/SystemApplication.java
@@ -1,7 +1,5 @@
 package com.gzu.system;
 
-import com.gzu.system.service.IUserLoginService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -9,7 +7,6 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 @ServletComponentScan
 public class SystemApplication {
-
 
     public static void main(String[] args) {
         SpringApplication.run(SystemApplication.class, args);
--- src/main/java/com/gzu/system/controller/PeopleController.java
+++ src/main/java/com/gzu/system/controller/PeopleController.java
@@ -1,17 +1,55 @@
 package com.gzu.system.controller;
 
+import com.gzu.system.service.PeopleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Controller()
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+
+@Controller
 public class PeopleController {
-    @GetMapping("/people")
-    public String index() {
-        return "redirect:/people/complete";
+
+    @Autowired
+    PeopleService peopleService;
+
+    @RequestMapping("/people")
+    public String index(HttpSession session) {
+        HashMap<String, String> userLoginMap = (HashMap<String, String>) session.getAttribute("userLoginMap");
+        if (peopleService.getInformation(userLoginMap.get("username")) == null) {
+            return "forward:/people/complete.html";
+        } else {
+            return "forward:/people/people.html";
+        }
     }
 
-    @GetMapping("/people/complete")
-    public String complete() {
-        return "forward:/people/complete.html";
+
+    @PostMapping("/people/post-complete-data")
+    public String peopleData(@RequestParam("full-name") String fullName, @RequestParam("phone-number") String phoneNumber,
+                             @RequestParam("id-card-number") String idCardNumber, @RequestParam("user-gender") String userGender,
+                             HttpSession session, RedirectAttributes redirectAttributes
+    ) {
+        HashMap<String, String> userLoginMap = (HashMap<String, String>) session.getAttribute("userLoginMap");
+        String username = userLoginMap.get("username");
+        if (peopleService.getInformation(username) == null) {
+            int completeStatus = peopleService.completeInformation(username, fullName, phoneNumber, idCardNumber, userGender);
+            switch (completeStatus) {
+                case 0:
+                    break;
+                case 1:
+                    redirectAttributes.addAttribute("error", "身份证被其他账号登记");
+                    break;
+                case 2:
+                    redirectAttributes.addAttribute("error", "发生错误");
+                    break;
+            }
+            return "redirect:/people";
+        }
+        throw new RuntimeException("发生错误");
     }
 }
