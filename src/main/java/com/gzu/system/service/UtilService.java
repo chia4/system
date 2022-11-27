79d170d3d5748d74be64764c5c5779da8245f229
Chia <lj1635@126.com>
2022-11-27 21:05:05 +0800

    提交需求给service层

---
+++ src/main/java/com/gzu/system/service/UtilService.java
@@ -0,0 +1,12 @@
+package com.gzu.system.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *   需求: 传入字符串内容，返回byte类型存放的png格式的图片
+ *   byte[] generateQRCode(String content, int width, int height)
+ */
+
+@Service
+public class UtilService {
+}
